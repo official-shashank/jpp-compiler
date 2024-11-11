ffca61af48b286ffd614e45b189fe670368fe5d4
official-shashank <shashankofficialedu@gmail.com>
2024-11-11 23:54:10 +0530

    some example uploaded

---
+++ js/data.js
@@ -0,0 +1,29 @@
+const webData={
+    "landingTitle":"huifghiufhiofhj"
+}
+
+console.log(webData["landingTitle"])
+
+const navbar=[
+
+    {
+        name:"Documentation",
+        href:"/documentation",
+    },
+    {
+        name:"Home",
+        href:"/home",
+    },
+    {
+        name:"Home",
+        href:"/home",
+    },
+    {
+        name:"Home",
+        href:"/home",
+    },
+    {
+        name:"Home",
+        href:"/home",
+    }
+]
