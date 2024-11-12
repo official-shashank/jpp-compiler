eef649f17dbebba249ae904103d788c75fbe5795
shivamtechtecho <shivamtechtecho@gmail.com>
2024-11-13 04:56:25 +0530

    commit3 day1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded",()=>{
     console.log("its working")
 })
+
+// tailwind.config.js  
+module.exports = {  
+    theme: {  
+      extend: {  
+        colors: {  
+          magenta: {  
+            500: '#ff00ff', // Example magenta color. Adjust as needed.  
+          },  
+        },  
+      },  
+    },  
+    variants: {},  
+    plugins: [],  
+  }
