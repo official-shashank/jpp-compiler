982ca38abd6b967df8c66bd327b3f0c2788ed17f
official-shashank <shashankofficialedu@gmail.com>
2024-11-13 11:40:12 +0530

    merged latest code

--- js/data.js
+++ js/data.js
@@ -1,29 +1,34 @@
-const webData={
-    "landingTitle":"huifghiufhiofhj"
-}
-
-console.log(webData["landingTitle"])
-
-const navbar=[
-
-    {
-        name:"Documentation",
-        href:"/documentation",
-    },
-    {
-        name:"Home",
-        href:"/home",
-    },
-    {
-        name:"Home",
-        href:"/home",
-    },
-    {
-        name:"Home",
-        href:"/home",
-    },
-    {
-        name:"Home",
-        href:"/home",
-    }
-]
+const matLangData = {
+  "Python Home": {
+    title: "Learn Python and More with data",
+    subtitle:
+      "Learn Python with our multilingual compiler and leverage your skills to shape the future. This documentation provides a hands-on approach to mastering any language effortlessly.",
+    Ques: [
+      {
+        Ques: "Learn Python",
+        sol: "Python is a popular programming language Python can be used on a server to create web applications.",
+      },
+    ],
+    code: [
+      {
+        code: `print("Hello world!")`,
+      },
+    ],
+  },
+  "Pyhton Intro":{
+    title: "Learn Python and More intro ",
+    subtitle:
+      "Learn Python with our multilingual compiler and leverage your skills to shape the future. This documentation provides a hands-on approach to mastering any language effortlessly.",
+    Ques: [
+      {
+        Ques: "Learn Python",
+        sol: "Python is a popular programming language Python can be used on a server to create web applications.",
+      },
+    ],
+    code: [
+      {
+        code: `print("Hello world!")`,
+      },
+    ],
+  },
+};
--- js/script.js
+++ js/script.js
@@ -1,18 +1,84 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    console.log("its working")
-})
+const pythonSidebar = [
+  "Python Home",
+  "Python Intro",
+  "Python Getting Started",
+  "Python Syntax",
+  "Python Component",
+  "Python Variable",
+  "Python Data Types",
+  "Python Numbers",
+  "Python Casting",
+  "Python Settings",
+  "Python Boolean",
+  "Python Operators",
+  "Python Lists",
+  "Python Tuples",
+  "Python Sets",
+  "Python Dictionaries",
+  "Python If....Else",
+  "Python While Loops",
+  "Python For Loops",
+  "Python Functions",
+  "Python Lamdba",
+  "Python Arrays",
+  "Python Classes/Objects",
+  "Python Inheritance",
+  "Python Iterators",
+  "Python Polymorphism",
+  "Python Scope",
+  "Python Modules",
+  "Python Dates",
+  "Python JSON",
+  "Python RegEx",
+  "Python PHP",
+  "Python Try...Except",
+  "Python User Input",
+  "Python String Formatting",
+];
 
-// tailwind.config.js  
-module.exports = {  
-    theme: {  
-      extend: {  
-        colors: {  
-          magenta: {  
-            500: '#ff00ff', // Example magenta color. Adjust as needed.  
-          },  
-        },  
-      },  
-    },  
-    variants: {},  
-    plugins: [],  
-  }
+const matLangSidebar=[
+
+]
+
+
+let sidebar=""
+
+function renderSidebar(sidebarData){
+
+
+
+  sidebarData.map((item,key)=>{
+
+      sidebar += `
+         <li key=${item} class="sidebar-item">
+            <a href="#" class="flex items-center p-2 text-gray-300 rounded-lg dark:text-white hover:bg-gray-700  dark:hover:bg-gray-700 group">
+               <span class="ms-3">${item}</span>
+            </a>
+         </li>
+      
+      `
+      document.getElementById("sidebarTarg").innerHTML=sidebar;
+  })
+
+}
+
+renderSidebar(pythonSidebar)
+
+
+let sidebarItems=document.getElementsByClassName("sidebar-item");
+
+for(let i=0;i<sidebarItems.length;i++){
+  sidebarItems[i].addEventListener("click",(e)=>{
+    console.log(e.target.innerText);
+    renderClickedContent(matLangData[e.target.innerText])
+  })
+}
+
+function renderClickedContent(data){
+  document.getElementById("title").innerText=data["title"]
+  document.getElementById("subtitle").innerText=data["subtitle"]
+   
+}
+
+
+
