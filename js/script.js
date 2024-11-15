35013fba5f9988ed281abb298e7fc1fa734095d8
shivamtechtecho <shivamtechtecho@gmail.com>
2024-11-15 20:29:37 +0530

    done my side tasks  day4

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 // // --------------------------------------------------------------------------------
-// // -----------------------------Only for Documentation Side-------------------------------
+// // -----------------------------Only for Documentation Sid-------------------------------
 
+const sideBar = document.getElementById("sidebarTarg");
+function renderSidebar(data) {
+  sideBar.innerHTML = "";
+  data.forEach((item, idx) => {
+    sideBar.innerHTML += `
+          <li class="sidebar-item">
+              <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-600 dark:hover:bg-gray-700 group">
+                 <span id="list${idx}" class="text-gray-400 highlight ms-3 ">${item}</span>
+              </a>
+           </li>
+          `;
 
-
+    // utility function
+    document
+      .getElementById(`list${idx}`)
+      .addEventListener("click", sideBarUtility(`${idx}`));
+  });
+}
 
 // MatLangCodeExamples
 
@@ -11,7 +27,7 @@ function showExampleMatlang() {
 
   exampleShowCaseMatLang.map((item) => {
     matres += `
-  <a href="../pages/docs.html" class=" flex flex-col mt-12 min-h-max h-96 max-w-md gap-4 p-4 rounded-lg transition-transform transform duration-300 ease-in-out hover:scale-110 animate-fade-in" style="box-shadow: 0 10px 25px 0 black">
+  <a href="../pages/docs.html" class=" flex flex-col mt-12 h-96 max-w-md gap-4 p-4 rounded-lg transition-transform transform duration-300 ease-in-out hover:scale-110 animate-fade-in" style="box-shadow: 0 10px 25px 0 black">
   <h2 class="text-white font-bold">${item.title}</h2>
   <span class="text-white">${item.exp}</span>
   <aside class="bg-black text-white p-6 rounded-lg w-full max-w-md font-mono max-h-56 overflow-y-auto stylish-scrollbar">
@@ -37,15 +53,37 @@ function showExampleMatlang() {
 
 document.addEventListener("DOMContentLoaded", () => {
   if (window.location.href.includes("/docs.html")) {
-    renderSidebar(matLangSidebar);
+    renderSidebar(pythonSidebar);
   }
   if (window.location.href.includes("/index.html")) {
     showExampleMatlang();
   }
-
 });
 
+// utility function
+function sideBarUtility(id) {}
 
+// Radio Button functionality
+// Function to check the initial selection on page load
+window.addEventListener("DOMContentLoaded", (event) => {
+  const radio1 = document.getElementById("default-radio-1");
+  const radio2 = document.getElementById("default-radio-2");
 
+  if (radio2.checked) {
+    renderSidebar(pythonSidebar);
+  } else if (radio1.checked) {
+    renderSidebar(matLangData);
+  }
 
+  radio1.addEventListener("change", () => {
+    if (radio1.checked) {
+      renderSidebar(pythonSidebar);
+    }
+  });
 
+  radio2.addEventListener("change", () => {
+    if (radio2.checked) {
+      renderSidebar(matLangSidebar);
+    }
+  });
+});
