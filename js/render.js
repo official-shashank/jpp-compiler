a2ee83331a9a0a1bac5b6dbf9e48d2486e5879f0
official-shashank <shashankofficialedu@gmail.com>
2024-11-16 13:00:16 +0530

    stashed changes"

--- js/render.js
+++ js/render.js
@@ -5,14 +5,17 @@ function getDataOfSelectedTag(){
 }
 
 function renderData(data){
-    document.getElementById("title")?.innerText=data["title"]
-    document.getElementById("subtitle")?.innerText=data["subtitle"]
+    document.getElementById("title").innerText=data["title"]
+    document.getElementById("subtitle").innerText=data["subtitle"]
+    
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const sidebarList = document.getElementsByClassName("sidebar-item");
+    
     Array.from(sidebarList).forEach(element => {
         element?.addEventListener("click", (e) => {
+            
             selectedTab=e.target.innerText;
             // console.log(selectedTab)
             let data = getDataOfSelectedTag();
